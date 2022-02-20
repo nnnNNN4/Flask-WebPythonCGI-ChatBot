997f92cfaead776827391dec182b5f201d3a055e
nnnNNN4 <koihime0musou@gmail.com>
2022-02-20 15:24:53 +0900

    docker

--- mysql/sqls/initialize.sql
+++ mysql/sqls/initialize.sql
@@ -1,2 +1,7 @@
-CREATE DATABASE hoge;
-use hoge;
+CREATE TABLE users (
+  id int(11) unsigned not null auto_increment,
+  name varchar(255) not null,
+  created_at datetime not null default current_timestamp,
+  updated_at datetime not null default current_timestamp on update current_timestamp,
+  primary key (id)
+);
