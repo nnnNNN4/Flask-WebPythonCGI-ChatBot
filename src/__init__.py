d2b0a8913edde9c1caaeee54abba050740648a0c
nnnNNN4 <koihime0musou@gmail.com>
2022-02-20 17:46:27 +0900

    url_for関数

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,6 @@
 import os
-
-from flask import Flask
+from flask import Flask, url_for
+from markupsafe import escape
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -19,26 +19,24 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    app = Flask(__name__)
+
     @app.route('/')
     def index():
-        return 'Index Page'
+        return 'index'
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    from markupsafe import escape
+    @app.route('/login')
+    def login():
+        return 'login'
 
     @app.route('/user/<username>')
-    def show_user_profile(username):
-        return 'User %s' % escape(username)
+    def profile(username):
+        return '{}\'s profile'.format(escape(username))
 
-    @app.route('/post/<int:post_id>')
-    def show_post(post_id):
-        return 'Post %d' % post_id
-
-    @app.route('/path/<path:subpath>')
-    def show_subpath(subpath):
-        return 'Subpath %s' % escape(subpath)
+    with app.test_request_context():
+        print(url_for('index'))
+        print(url_for('login'))
+        print(url_for('login', next='/'))
+        print(url_for('profile', username='hogehoge'))
 
     return app
